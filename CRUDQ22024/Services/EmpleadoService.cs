f3f7b3b7d665f9edc0d731c136aeaeb16c88f4af
gurdian0614 <ivan.gurdian@outlook.com>
2024-06-05 19:27:05 -0600

    Creacion de servicio empleado

---
+++ CRUDQ22024/Services/EmpleadoService.cs
@@ -0,0 +1,33 @@
+﻿
+using CRUDQ22024.Models;
+using SQLite;
+
+namespace CRUDQ22024.Services
+{
+    public class EmpleadoService
+    {
+        private readonly SQLiteConnection dbConnection;
+
+        public EmpleadoService()
+        {
+            // Construir ruta
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Empleado.db3");
+            
+            //Inicializamos el objeto
+            dbConnection = new SQLiteConnection(dbPath);
+            
+            //Creamos la tabla Empleado
+            dbConnection.CreateTable<Empleado>();
+        }
+
+        public List<Empleado> GetAll() {
+            var res = dbConnection.Table<Empleado>().ToList();
+            return res;
+        }
+
+        public int Insert(Empleado empleado)
+        {
+            return dbConnection.Insert(empleado);
+        }
+    }
+}
