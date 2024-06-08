369865b3c39c7e2a5a871b046c8fd4fe9bad9324
gurdian0614 <ivan.gurdian@outlook.com>
2024-06-07 18:18:53 -0600

    Completando CRUD en servicios

--- CRUDQ22024/Services/EmpleadoService.cs
+++ CRUDQ22024/Services/EmpleadoService.cs
@@ -20,14 +20,33 @@ namespace CRUDQ22024.Services
             dbConnection.CreateTable<Empleado>();
         }
 
+        /// <summary>
+        /// Lista todos los empleados
+        /// </summary>
+        /// <returns>Una lista de empleados</returns>
         public List<Empleado> GetAll() {
             var res = dbConnection.Table<Empleado>().ToList();
             return res;
         }
 
+        /// <summary>
+        /// Guarda un empleado a la base de datos
+        /// </summary>
+        /// <param name="empleado">Objeto con datos del empleado a guardar</param>
+        /// <returns>Cantidad de registros ingresados</returns>
         public int Insert(Empleado empleado)
         {
             return dbConnection.Insert(empleado);
+        }
+
+        /// <summary>
+        /// Actualiza un registro seleccionado
+        /// </summary>
+        /// <param name="empleado">Objeto con datos del empleado a actualizar</param>
+        /// <returns>Cantidad de resgistros actualizados</returns>
+        public int Update(Empleado empleado) 
+        {
+            return dbConnection.Update(empleado);
         }
     }
 }
