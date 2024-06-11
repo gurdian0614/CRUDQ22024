0b651bfb4f35a70a820a1d23e30612970022f118
gurdian0614 <ivan.gurdian@outlook.com>
2024-06-10 19:25:28 -0600

    Inicializando view model

--- CRUDQ22024/Views/EmpleadoMain.xaml.cs
+++ CRUDQ22024/Views/EmpleadoMain.xaml.cs
@@ -1,9 +1,20 @@
+using CRUDQ22024.ViewModels;
+
 namespace CRUDQ22024.Views;
 
 public partial class EmpleadoMain : ContentPage
 {
+	private EmpleadoMainViewModel viewModel;
 	public EmpleadoMain()
 	{
 		InitializeComponent();
+		viewModel = new EmpleadoMainViewModel();
+		this.BindingContext = viewModel;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		viewModel.GetAll();
+    }
 }
